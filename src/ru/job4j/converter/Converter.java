ed1085c69eb73e8c359a0289ae5c6ce634af7563
Rodion <novicovrv@gmail.com>
2020-10-06 15:58:11 +0300

    9.1. String [#375071]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -3,18 +3,20 @@ package ru.job4j.converter;
 public class Converter {
     public static int rubleToDollar(int value) {
         int r = value / 60;
-        return r;
+        return value / 60;
     }
+
     public static int rubleToEuro(int value) {
         int r = value / 70;
-        return r;
+        return value / 70;
     }
+
     public static void main(String[] args) {
-        int dollar = Converter.rubleToDollar( 60 );
+        int dollar = Converter.rubleToDollar(60);
         int euro = Converter.rubleToEuro(70);
 
         System.out.println("60 rubles are " + dollar + " dollar.");
-        System.out.println("70 rubles are " + euro + "euro" );
+        System.out.println("70 rubles are " + euro + "euro");
     }
 }
 
