e0991ef5493534a4cd91b71652ddbe84b8af850c
Rodion <novicovrv@gmail.com>
2020-10-07 15:15:43 +0300

    Тесты для определения расстояния между точками. [#375095]

---
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SqAreaTest {
+
+    @Test
+    public void square1() {
+        int expected = 2;
+        int p = 18;
+        double out = SqArea.sqArea(p, 6);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void square2() {
+        int expected = 6;
+        int k = 6;
+        double out = SqArea.sqArea(19, k);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
