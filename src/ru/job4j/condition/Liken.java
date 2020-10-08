38f27a1f9f2a86f336b5bbd2b2f2e81833e11a67
Rodion <novicovrv@gmail.com>
2020-10-08 11:11:22 +0300

    0. Операции сравнения [#375009]

---
+++ src/ru/job4j/condition/Liken.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+public class Liken {
+
+    public static void main(String[] args) {
+        int first = 18;
+        int second = 21;
+        boolean result = first > second;
+        System.out.println(result);
+
+        result = first < second;
+        System.out.println(result);
+
+        result = first == second;
+        System.out.println(result);
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
