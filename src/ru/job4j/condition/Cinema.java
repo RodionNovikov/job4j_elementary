e9a55973da8ffe49a5e0f2c8e43710b332ee9def
Rodion <novicovrv@gmail.com>
2020-10-08 15:57:53 +0300

    Оператор if с блоком else [#375008]

---
+++ src/ru/job4j/condition/Cinema.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+public class Cinema {
+
+    public static void access(int age) {
+        System.out.println("The age of customer is" + age);
+        if (age >= 18) {
+            System.out.println("Welcome on bord");
+        } else {
+            System.out.println("Go home baby");
+
+        }
+    }
+
+    public static void main(String[] args) {
+            access(15);
+
+    }
+}
+
