425a815bb7caa3dbf2d9a640dcef84abde65c3ae
Rodion <novicovrv@gmail.com>
2020-10-16 12:54:05 +0300

    5.6. Простые числа [#375118]

---
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+
+    public static int calc(int finish) {
+        int count = 0;
+        for (int index = 1; index <= finish; index++) {
+            if (CheckPrimeNumber.check(index)) {
+                count += 1;
+            } else {
+                count += 0;
+            }
+        }
+        return count;
+    }
+}
+
+
+
