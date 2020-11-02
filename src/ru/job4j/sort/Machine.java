ffed810959d73546136ec84f6d42040ca50ead06
Rodion <novicovrv@gmail.com>
2020-11-02 17:03:59 +0300

    6.9. Жадный алгоритм. Сдача в кофе машине [#375029]

--- src/ru/job4j/sort/Machine.java
+++ src/ru/job4j/sort/Machine.java
@@ -3,8 +3,7 @@ import java.util.Arrays;
 
 public class Machine {
     private final int[] coins = {10, 5, 2, 1};
-
-    public int[] change(int money, int price) {
+        public int[] change(int money, int price) {
         int[] rsl = new int[100];
         int size = 0;
         int chenge = money - price;
